e8e26d56ffcb00b9700be364dfc13971648c61e3
Arthur Silva <arthur.gbs.dev@gmail.com>
2025-12-15 13:07:42 -0300

    Criação de metodos adicionarLivro(), removerLivro() e buscarLivro(). Realização de testes de validação dos metodos via JUnit5

--- src/model/Biblioteca.java
+++ src/model/Biblioteca.java
@@ -10,5 +10,73 @@ public class Biblioteca {
 
     public Biblioteca() {}
 
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void removerLivro(String tituloLivro) {
+
+        Iterator<Livro> iterator = livros.iterator();
+
+        while (iterator.hasNext()) {
+            Livro livro = iterator.next();
+
+            if (livro.getTitulo().equalsIgnoreCase(tituloLivro)){
+                iterator.remove();
+
+                return;
+            }
+        }
+    }
+
+    public void removerLivro(Integer id) {
+
+        Iterator<Livro> iterator = livros.iterator();
+
+        while (iterator.hasNext()) {
+            Livro livro = iterator.next();
+
+            if (id == livro.getIdLivro()){
+                iterator.remove();
+
+                return;
+            }
+        }
+    }
+
+    public Livro buscarLivro(String tituloLivro) {
+
+        Iterator<Livro> iterator = livros.iterator();
+
+        do {
+            Livro livro = iterator.next();
+
+            if (livro.getTitulo().equalsIgnoreCase(tituloLivro)){
+
+                return livro;
+            } else {
+                return null;
+            }
+        } while (iterator.hasNext());
+
+    }
+
+    public Livro buscarLivro(Integer idLivro) {
+
+        Iterator<Livro> iterator = livros.iterator();
+
+        do {
+            Livro livro = iterator.next();
+
+            if (idLivro == livro.getIdLivro()){
+                return livro;
+            } else {
+                return null;
+            }
+        } while (iterator.hasNext());
+
+    }
+
+
 
 }
--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -1,8 +1,46 @@
 package test.java;
 
+import model.Biblioteca;
+import model.Livro;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BibliotecaTest {
 
+    private Livro livro = new Livro(1, "Harry Potter", "J.K Rowling");
+    private Biblioteca biblioteca = new Biblioteca();
+
+    @Test
+    void testeAdicionarLivro() {
+        biblioteca.adicionarLivro(livro);
+        assertTrue(biblioteca.livros.contains(livro));
+    }
+
+    @Test
+    void removerLivroPorTitulo() {
+        biblioteca.adicionarLivro(livro);
+        biblioteca.removerLivro(livro.getTitulo());
+        assertFalse(biblioteca.livros.contains(livro));
+    }
+
+    @Test
+    void removerLivroPorId() {
+        biblioteca.adicionarLivro(livro);
+        biblioteca.removerLivro(livro.getIdLivro());
+        assertFalse(biblioteca.livros.contains(livro));
+    }
+
+    @Test
+    void buscarLivroPorTitulo() {
+        biblioteca.adicionarLivro(livro);
+        boolean retornouLivro = biblioteca.buscarLivro(livro.getTitulo()) != null && biblioteca.buscarLivro(livro.getTitulo()) == livro;
+        assertTrue(retornouLivro);
+    }
+
+    @Test
+    void buscarLivroPorId() {
+        biblioteca.adicionarLivro(livro);
+        boolean retornouLivro = biblioteca.buscarLivro(livro.getIdLivro()) != null && biblioteca.buscarLivro(livro.getIdLivro()) == livro;
+        assertTrue(retornouLivro);
+    }
 }
